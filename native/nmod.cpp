f51763307fc00ecb5596d146ab87551f91f8bc01
dixeran <dixeran@outlook.com>
2019-09-27 23:22:46 +0800

    rudeless

--- native/nmod.cpp
+++ native/nmod.cpp
@@ -34,44 +34,10 @@ Napi::Value Play(const Napi::CallbackInfo &info)
     return env.Null();
 }
 
-Napi::Value FuckFrameless(const Napi::CallbackInfo &info)
-{
-    Napi::Env env = info.Env();
-    int64_t wid = info[0].As<Napi::Number>().Int32Value();
-    HWND windowHandle = (HWND)wid;
-    /*frameless window musn't have WS_THICKFRAME*/
-    // LONG_PTR style = GetWindowLongPtr(windowHandle, GWL_STYLE);
-    // style = style | WS_THICKFRAME;
-    // SetWindowLongPtr(windowHandle, GWL_STYLE, style);
-
-    // style = GetWindowLongPtr(windowHandle, GWL_EXSTYLE);
-    // style = style | WS_EX_TRANSPARENT;
-    // SetWindowLongPtr(windowHandle, GWL_EXSTYLE, style);
-
-    HWND child = FindWindowEx(windowHandle, NULL, NULL, NULL);
-    if (child != NULL)
-    {
-        wid = (int64_t)child;
-        mpv_set_property(ctx, "wid", MPV_FORMAT_INT64, &wid);
-        mpv_initialize(ctx);
-        const char *cmd[] = {"loadfile", "test.mp4", NULL};
-        mpv_command(ctx, cmd);
-    }
-    else
-    {
-        mpv_set_property(ctx, "wid", MPV_FORMAT_INT64, &wid);
-        mpv_initialize(ctx);
-        const char *cmd[] = {"loadfile", "test.mp4", NULL};
-        mpv_command(ctx, cmd);
-    }
-    return env.Null();
-}
-
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
     exports.Set(Napi::String::New(env, "init"), Napi::Function::New(env, InitMpv));
     exports.Set(Napi::String::New(env, "play"), Napi::Function::New(env, Play));
-    exports.Set(Napi::String::New(env, "fuck"), Napi::Function::New(env, FuckFrameless));
     return exports;
 }
 
